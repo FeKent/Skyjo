8278178c2f1a56e634390e88bd68c0edd8ccbd3d
FeKent <kentf022@gmail.com>
2025-02-15 23:43:08 +0000

    Setup GameState interfaces

--- engine/src/main/GameState.kt
+++ engine/src/main/GameState.kt
@@ -7,4 +7,28 @@ import kotlinx.serialization.Serializable
 @Serializable
 sealed interface GameState {
 
+    @Serializable
+    @SerialName("NotStarted")
+    class NotStated : GameState {
+
+    }
+
+    @Serializable
+    sealed interface StartedGameState : GameState {
+
+        @Serializable
+        sealed interface LiveGameState : GameState {
+
+
+            @Serializable
+            @SerialName("AwaitingInitialFlip")
+            class AwaitingInitialFlip(
+                //where you wait for everyone to flip over their initial two cards
+                //then whoever had the highest flip total would go first
+            )
+
+
+        }
+
+    }
 }
