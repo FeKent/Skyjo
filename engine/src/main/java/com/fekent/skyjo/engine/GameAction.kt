ac00799b02b8fe6e123f59f337424f096b4857a5
FeKent <kentf022@gmail.com>
2025-02-24 18:19:40 +0000

    Add Joined and Left Lobby data class GameActions

--- engine/src/main/java/com/fekent/skyjo/engine/GameAction.kt
+++ engine/src/main/java/com/fekent/skyjo/engine/GameAction.kt
@@ -1,12 +1,33 @@
 package com.fekent.skyjo.engine
 
+import jdk.jfr.Timestamp
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 
 @Serializable
-sealed interface GameAction{
+sealed interface GameAction {
 
     val id: String
     val timestamp: String
     val playerId: PlayerId
+
+    @Serializable
+    @SerialName("JoinedLobby")
+    data class JoinedLobby(
+        override val id: String,
+        override val timestamp: String,
+        override val playerId: PlayerId,
+        val playerName: String,
+    ) : GameAction
+
+    @Serializable
+    @SerialName("LeftLobby")
+    data class LeftLobby(
+        override val id: String,
+        override val timestamp: String,
+        override val playerId: PlayerId,
+    ) : GameAction
+
+
 }
