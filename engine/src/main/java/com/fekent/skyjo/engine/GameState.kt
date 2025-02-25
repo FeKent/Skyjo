7af6af620f4a4b5e2f51cd337dafe9b89eb5a4ab
FeKent <kentf022@gmail.com>
2025-02-25 21:56:13 +0000

    Add initialGameState and needed values

--- engine/src/main/java/com/fekent/skyjo/engine/GameState.kt
+++ engine/src/main/java/com/fekent/skyjo/engine/GameState.kt
@@ -7,14 +7,30 @@ import kotlinx.serialization.Serializable
 @Serializable
 sealed interface GameState {
 
+    companion object {
+        val initialGameState: GameState = NotStarted(allPlayers = emptyList())
+    }
+
+    val allPlayers: List<Player>
+
+    val allPlayerIds: List<PlayerId> get() = allPlayers.map(Player::id)
+
+    val allPlayerNames: List<String> get() = allPlayers.map(Player::name)
+
+    fun getPlayer(playerId: PlayerId): Player? {
+        return allPlayers.find { it.id == playerId }
+    }
+
     @Serializable
     @SerialName("NotStarted")
-    class NotStarted : GameState {
-
+    class NotStarted(override val allPlayers: List<Player>) : GameState {
+        override fun toString() = "NotStarted"
     }
 
     @Serializable
     sealed interface StartedGameState : GameState {
+
+        override val allPlayers: List<Player>
 
         @Serializable
         sealed interface LiveGameState : StartedGameState {
@@ -22,6 +38,7 @@ sealed interface GameState {
             @Serializable
             @SerialName("AwaitingRoundStart")
             class AwaitingRoundStart(
+                override val allPlayers: List<Player>
                 //wait for everyone to flip over their initial two cards
                 //then whoever had the highest flip total would go first OR who finished previous round
                 //this starts the round!
@@ -34,12 +51,14 @@ sealed interface GameState {
                 @Serializable
                 @SerialName("AwaitingDrawDecision")
                 class AwaitingDrawDecision(
+                    override val allPlayers: List<Player>
                     //wait for player to choose either the revealed (pile) or unrevealed card (deck)
                 ): RoundGameState
 
                 @Serializable
                 @SerialName("AwaitingPlayDecision")
                 class AwaitingPlayDecision(
+                    override val allPlayers: List<Player>
                     //wait for player to decided whether to keep drawn card and where it's placed
                     //OR whether to discard the drawn card and flip an unrevealed card in hand
                     //either choice means a card will be put in the pile
@@ -48,6 +67,7 @@ sealed interface GameState {
                 @Serializable
                 @SerialName("AwaitingFlipDecision")
                 class AwaitingFlipDecision(
+                    override val allPlayers: List<Player>
                     //wait for player to decide position of new card on their board
                     //new card will either be the drawn card or one from the players hand
                 ): RoundGameState
@@ -55,6 +75,7 @@ sealed interface GameState {
                 @Serializable
                 @SerialName("AwaitingSkyjo")
                 class AwaitingSkyjo(
+                    override val allPlayers: List<Player>
                     //if player's new card means that there is a column of REVEALED same numbers
                     //then that row will ALL be moved onto the pile, with the initial discarded card on the bottom
                 ): RoundGameState
