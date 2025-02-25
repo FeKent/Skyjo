316ade6f8cc1d0ef67263493b9f8f8af2ce577a4
FeKent <kentf022@gmail.com>
2025-02-25 21:42:48 +0000

    Rename initial class and define type of state for subsequent classes

--- engine/src/main/java/com/fekent/skyjo/engine/GameState.kt
+++ engine/src/main/java/com/fekent/skyjo/engine/GameState.kt
@@ -9,7 +9,7 @@ sealed interface GameState {
 
     @Serializable
     @SerialName("NotStarted")
-    class NotStated : GameState {
+    class NotStarted : GameState {
 
     }
 
@@ -17,7 +17,7 @@ sealed interface GameState {
     sealed interface StartedGameState : GameState {
 
         @Serializable
-        sealed interface LiveGameState : GameState {
+        sealed interface LiveGameState : StartedGameState {
 
             @Serializable
             @SerialName("AwaitingRoundStart")
@@ -25,7 +25,7 @@ sealed interface GameState {
                 //wait for everyone to flip over their initial two cards
                 //then whoever had the highest flip total would go first OR who finished previous round
                 //this starts the round!
-            )
+            ) : LiveGameState
 
             @Serializable
             sealed interface RoundGameState : GameState{
@@ -35,7 +35,7 @@ sealed interface GameState {
                 @SerialName("AwaitingDrawDecision")
                 class AwaitingDrawDecision(
                     //wait for player to choose either the revealed (pile) or unrevealed card (deck)
-                )
+                ): LiveGameState
 
                 @Serializable
                 @SerialName("AwaitingPlayDecision")
@@ -43,21 +43,21 @@ sealed interface GameState {
                     //wait for player to decided whether to keep drawn card and where it's placed
                     //OR whether to discard the drawn card and flip an unrevealed card in hand
                     //either choice means a card will be put in the pile
-                )
+                ): LiveGameState
 
                 @Serializable
                 @SerialName("AwaitingFlipDecision")
                 class AwaitingFlipDecision(
                     //wait for player to decide position of new card on their board
                     //new card will either be the drawn card or one from the players hand
-                )
+                ): LiveGameState
 
                 @Serializable
                 @SerialName("AwaitingSkyjo")
                 class AwaitingSkyjo(
                     //if player's new card means that there is a column of REVEALED same numbers
                     //then that row will ALL be moved onto the pile, with the initial discarded card on the bottom
-                )
+                ): LiveGameState
 
             }
         }
