6e3a5ccbddb26d875ef65884d4906cb0ea7ce67a
FeKent <kentf022@gmail.com>
2025-02-28 16:06:15 +0000

    Test: Verify generation of player deck

--- engine/src/test/java/com/fekent/engine/DeckTest.kt
+++ engine/src/test/java/com/fekent/engine/DeckTest.kt
@@ -1,5 +1,7 @@
 package com.fekent.engine
 
+import com.fekent.skyjo.engine.Board
+import com.fekent.skyjo.engine.Rules
 import com.fekent.skyjo.engine.Rules.fullDeck
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
@@ -7,7 +9,7 @@ import org.junit.Test
 class DeckTest {
 
     @Test
-    fun fullDeckWith150Cards(){
+    fun fullDeckWith150Cards() {
         fullDeck
 
         assertEquals(150, fullDeck.cards.size)
@@ -20,5 +22,15 @@ class DeckTest {
         val randomBoard = shuffledDeck.take(12)
         println("Shuffled Board:  ${randomBoard.joinToString { it.value.toString() }}")
 
+    }
+
+    @Test
+    fun generatePlayerBoardFromDeck() {
+        val emptyBoard = Board(emptyList())
+        val playerBoard = emptyBoard.startingBoard(fullDeck.cards.shuffled())
+
+        println("Starting Board Cards: ${playerBoard.cards.joinToString { it.value.toString() }}")
+
+        assertEquals(Rules.initialBoardSize, playerBoard.cards.size)
     }
 }
