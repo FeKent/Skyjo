e77deaf7a16ad0a73a504a2abdcad45476c54e0d
FeKent <kentf022@gmail.com>
2025-03-01 15:26:40 +0000

    Add rule functions for dealing cards

--- engine/src/main/java/com/fekent/skyjo/engine/Rules.kt
+++ engine/src/main/java/com/fekent/skyjo/engine/Rules.kt
@@ -16,8 +16,31 @@ object Rules {
                 (-1..12)
                     .filter { it != 0 }
                     .forEach { _ -> repeat(10) { add(Card(it)) } }
-            }
+            }.shuffled()
         )
 
     const val initialBoardSize: Int = 12
+
+    fun takeBoardFromDeck(deck: Deck): Pair<Board, Deck> {
+        val (boardCards, newDeck) = takeTilesFromBag(deck = deck, count = initialBoardSize)
+        return Board(boardCards) to newDeck
+    }
+
+    fun takeTilesFromBag(deck: Deck, count: Int): Pair<List<Card>, Deck> {
+        return with(deck.cards.shuffled()) { take(count) to Deck(drop(count)) }
+    }
+
+    fun deal(playerIds: List<PlayerId>): Map<PlayerId, Board> {
+        var deck = fullDeck
+        val boards = mutableMapOf<PlayerId, Board>()
+
+        for (playerId in playerIds) {
+            val (board, newDeck) = takeBoardFromDeck(deck)
+            deck = newDeck
+            boards[playerId] = board
+        }
+
+        return boards
+    }
+
 }
